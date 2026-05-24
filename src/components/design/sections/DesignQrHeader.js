d0a863c3808e5724307d4288a89c173933031ef4
Muhammad Abdullah Rauf <abdullahrauf440@gmail.com>
2026-05-25 03:11:55 +0500

    fixed

--- src/components/design/sections/DesignQrHeader.js
+++ src/components/design/sections/DesignQrHeader.js
@@ -5,15 +5,15 @@ import { BRAND } from "@/lib/brand";
 
 export function DesignQrHeader() {
   return (
-    <header className="relative z-10 pb-0 max-md:pb-0 md:pb-1">
-      <Container className="flex flex-col items-center px-4 pt-2 text-center max-md:pt-1.5 max-md:pb-0 sm:pt-3">
+    <header className="relative z-10 pb-2">
+      <Container className="flex flex-col items-center gap-0 px-4 text-center">
         <Image
           src="/images/YardCraftLogo.png"
           alt={BRAND.name}
           width={260}
           height={260}
+          style={{ transform: "scale(2)" }}
           priority
-          className="h-[3.25rem] w-auto sm:h-[4.25rem] lg:h-[5.25rem]"
         />
       </Container>
     </header>
